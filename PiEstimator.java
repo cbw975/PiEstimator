c096c61addd4ca0f6e162cb7e7a22b4015ec4d27
cwohlgemuth22 <cwohlgemuth22@amherst.edu>
2021-02-16 15:48:10 -0500

    Implement MC Sim code + Threads

--- PiEstimator.java
+++ PiEstimator.java
@@ -6,17 +6,19 @@
   *
   * @author Chloe Wohlgemuth
 */
-
   public class PiEstimator {
     // Fields
     private long numPoints = 0; // number of points or samples
-    private int numThreads = 1; // number of threads (to distribute samples over)
+    private int numThreads = 1; // number of threads to run (distribute samples over)
+    private double pi; // estimated value of pi
+
 
     /** Constructor. Takes @param numPoints number of sample points and @param numThreads
      * number of threads used to compute the estimate */
     public PiEstimator (long numPoints, int numThreads) {
 	    this.numPoints = numPoints;
         this.numThreads = numThreads;
+        this.pi = 0;
     }
 
     /** Computes the estimate of pi
@@ -24,6 +26,29 @@
      * @return the number of samples that lie inside a disk inscribed in the square. Pi can
      * be estimated from the proportion of sample that fall in the disk. */
     public double getPiEstimate () {
-	    ...
+        long[] piEstimates = new long[numThreads]; // shared array with an index for each thread
+	    Thread[] threads = new Thread[numThreads]; // array of threads
+
+        // Initialize threads with the specified number of samples evenly distributed across
+        for(int i=0; i<numThreads; i++){
+            threads[i] = new Thread(new PiThread((long)(numPoints/numThreads),i,piEstimates));
+        }
+
+        // start all of the threads
+        for (Thread t : threads) {
+            t.start();
+        }
+
+        // wait for all threads to complete
+        for (Thread t : threads) {
+            try {
+                t.join();
+            } catch (InterruptedException ignored) {} // don't care if it gets interrupted
+        }
+
+        //pi value = sum of points that were made in circle (sum of array) / total number of points * 4
+        long pointsInCircle = 0; // number of points in inscribed circle
+        for (long e : piEstimates) pointsInCircle += e;
+        return (double) pointsInCircle / numPoints * 4.0;
     }
 }
--- PiThread.java
+++ PiThread.java
@@ -3,19 +3,28 @@
   *
   * @author Chloe Wohlgemuth
 */
-
 import java.util.concurrent.ThreadLocalRandom;
 
-public class PiThread implements Runnable {
-    public static final int numThreads = 4;  // number of threads to run
-    private long numSamples = 0;
+class PiThread implements Runnable {
+    private long numPoints = 0;                // number of samples or points
+    private int id;                           // thread id
+    private long[] array;                      // array
 
-    public PiThread(numSamples){
-        this.numSamples = numSamples;
+    public PiThread(long numPoints, int id, long[] array){
+        this.numPoints = numPoints; 
+        this.id = id;
+        this.array = array;
     }
-
+    
     public void run () {
-        
+        long pointsInCircle = 0; // number of points in inscribed circle
+        for(int i=0; i<numPoints; i++){
+            double x = ThreadLocalRandom.current().nextDouble(); //coord position of point
+            double y = ThreadLocalRandom.current().nextDouble(); //coord position of point
+            if( x*x+y*y <= 1){
+                pointsInCircle += 1;
+            }
+        }
+        array[id] = pointsInCircle; // set estimated pi value at my ID's index
     }
-
 }
